10d7089354d446c05f5e7845a10d243724570421
christianlincke <christian.lincke@gmx.de>
2026-07-28 13:46:07 +0200

    implement cli game

--- src/cli/main.c
+++ src/cli/main.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
-
+#include "core/textbuffer.h"
 #include "core/grid.h"
+#include "kbdinput.h"
+#include "render.h"
 
 int main(int argc, char *argv[])
 {
-    for (int i = 5; i >= 0; i--) {
-        printf("%d\n", i);
+    TextBuffer buffer;
+    tb_init(&buffer);
+
+    Grid grid;
+    grid_init(&grid);
+    grid_newCell(&grid);
+    grid_newCell(&grid);
+    grid_dump(&grid, &buffer);
+    tb_dump(&buffer);
+
+    while (1) {
+        int ch = getch();
+        switch (ch)
+        {
+        case 'w':
+            grid_push(&grid, GRID_UP);
+            grid_newCell(&grid);
+            break;
+        case 'a':
+            grid_push(&grid, GRID_LEFT);
+            grid_newCell(&grid);
+            break;
+        case 's':
+            grid_push(&grid, GRID_DOWN);
+            grid_newCell(&grid);
+            break;
+        case 'd':
+            grid_push(&grid, GRID_RIGHT);
+            grid_newCell(&grid);
+            break;
+        case 'q':
+            return 0;
+            break;
+        default:
+            break;
+        }
+        
+        grid_dump(&grid, &buffer);
+        tb_dump(&buffer);
     }
 
     return 0;
